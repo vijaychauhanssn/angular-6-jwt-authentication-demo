faddd693b9f40985dc9c6f6d3062dfbeff5da7f8
vijaychauhanssn <vijaychauhan1994@outlook.com>
2019-03-24 10:01:45 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-6-jwt-authentication-demo
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-6-jwt-authentication-demo)
---
+++ src/app/app.routing.ts
@@ -0,0 +1,14 @@
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home';
+import { LoginComponent } from './login';
+import { AuthGuard } from './guards';
+
+const appRoutes: Routes = [
+    { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+    { path: 'login', component: LoginComponent },
+
+    // otherwise redirect to home
+    { path: '**', redirectTo: '' }
+];
+
+export const routing = RouterModule.forRoot(appRoutes);
