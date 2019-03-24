338776e0d33c0cd6937b878336e429518360d392
vijaychauhanssn <vijaychauhan1994@outlook.com>
2019-03-24 11:42:31 +0530

    1 file modified

--- src/app/helpers/fake-backend.ts
+++ src/app/helpers/fake-backend.ts
@@ -9,7 +9,7 @@ export class FakeBackendInterceptor implements HttpInterceptor {
     constructor() { }
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        let testUser = { id: 1, username: 'test', password: 'test', firstName: 'Test', lastName: 'User' };
+        let testUser = { id: 1, username: 'demo', password: 'demo123', firstName: 'Test', lastName: 'User' };
 
         // wrap in delayed observable to simulate server api call
         return of(null).pipe(mergeMap(() => {
